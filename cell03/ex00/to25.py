cc8580ec1d866d2f502578f6303b19c138f6e1c3
Rawikarn-nuea <rawinkarn2549boon@gmail.com>
2025-06-21 10:33:56 +0000

    done 3.0

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+user_input_num = int(input( " Enter a number less than 25 "))
+# var = input("prompt")
+
+if user_input_num < 25 :
+    count = user_input_num
+    while count <= 25:
+        print(f"Inside the loop, my variable is {count}")
+        count += 1
+else :
+    print ("Error")
