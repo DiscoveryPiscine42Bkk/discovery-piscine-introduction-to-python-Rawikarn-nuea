7f5e23fa06bba1d34777eadc99f8ae9ee37fd1df
Rawikarn-nuea <rawinkarn2549boon@gmail.com>
2025-06-22 14:37:22 +0000

    5.10 not done

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+if len(sys.argv) != 2:
+    print("none")
+else:
+    pram = sys.argv[1]
+    answer = input("What was the parameter? ")
+    if answer == param:
+        print("Good job")
+    else:
+        print("Nope, sorry...")
